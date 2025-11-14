0f2602087583c6c80d5643ebe3df1ee9c7d68aff
jesusVazquez21 <jesus_3141240253@utd.edu.mx>
2025-11-14 12:51:03 -0600

    prueba 2

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from view.plantilla.plantilla_interfaz import *
 from controller.funciones import *
 from view.ventas.ventas import *
 #prueba jeje hjjnk ouyea yisuscambios
+
+#Segunda prueba ksadhsjka
 class login(Frame):#Cada interfaz es un Frame. La clase hereda los atributos y metodos de la clase Frame()
     def __init__(self, master, controlador): #El master es el contenedor padre del widget o frame. En todas las interfaces sera la ventana App()
         super().__init__(master) #Se heredan los atributos que tenga la clase App. 
